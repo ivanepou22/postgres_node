725784614dd297f930dd1c9a9df60377c08830e3
Ivan Epou <ivanepou92@gmail.com>
2023-01-08 14:43:47 +0300

    Add routes txt file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app: express.Application = express();
+
+//configure cors
+const corsOptions = {
+  origin: 'http://someotherdomain.com',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 
 //port
 const { PORT } = process.env;
@@ -12,6 +23,14 @@ const { PORT } = process.env;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Express');
 });
+
+app.get(
+  '/test-cors',
+  cors(corsOptions),
+  (req: Request, res: Response, next: express.NextFunction) => {
+    res.json({ msg: 'This cors is enabled with a middle ware' });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
