7a907466714c4d10e27c29535df1f82caafeebbe
Ivan Epou <ivanepou92@gmail.com>
2023-01-09 17:01:47 +0300

    Generate user migrations

---
+++ migrations/sqls/20230109131700-users-table-up.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
